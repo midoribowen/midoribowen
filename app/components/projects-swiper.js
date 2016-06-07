c54e6d6d87d4b762e0841c863ee26d36092dd2cd
Midori Bowen <midori.p.bowen@gmail.com>
2016-06-06 20:35:02 -0700

    Access images through switch case.

--- app/components/projects-swiper.js
+++ app/components/projects-swiper.js
@@ -1,6 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  slideImage1: "",
+  slideImage1Alt: "",
+  slideImage2: "",
+  slideImage2Alt: "",
+  slideImage3: "",
+  slideImage3Alt: "",
   slideTitle: "",
   slideDescription: "",
   slideSource: "",
@@ -10,6 +16,12 @@ export default Ember.Component.extend({
     swipeContent(properties) {
       switch (properties.attributes[2].value) {
         case "0":
+          this.set("slideImage1", "assets/img/SIU-1.png");
+          this.set("slideImage1Alt", "Image of Shake It Up Android App");
+          this.set("slideImage2", "assets/img/SIU-2.png");
+          this.set("slideImage2Alt", "Image of Shake It Up Android App");
+          this.set("slideImage3", "assets/img/SIU-3.png");
+          this.set("slideImage3Alt", "Image of Shake It Up Android App");
           this.set("slideTitle", "Shake It Up!");
           this.set("slideDescription", "An Android app that helps users decide on their evening plans. It uses animations, gesture detection, fragments, implicit intents, and the Yelp and GoogleMaps APIs.");
           this.set("slideSource", "https://github.com/midoribowen/ShakeItUp")
